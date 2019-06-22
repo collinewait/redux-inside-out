9a6e04d29c1a53ddd434a6a4ecba8a12f79b94a2
collinewait <collinewait17@gmail.com>
2019-06-22 16:08:43 +0300

    log state before and after increment dispatch

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
-import reducers from './reducer.js';
+import counterReducer from './reducer.js';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducers, composeWithDevTools());
+const store = createStore(counterReducer, composeWithDevTools());
+console.log(store.getState());
+store.dispatch({ type: 'INCREMENT'});
+console.log(store.getState());
 export default store;
