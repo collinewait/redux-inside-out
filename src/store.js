f4c2f291819d92ee3496f7385eb14df2d438db33
collinewait <collinewait17@gmail.com>
2019-06-22 16:59:11 +0300

    implement createStore from scratch

--- src/store.js
+++ src/store.js
@@ -1,12 +1,36 @@
-import { createStore } from "redux";
+//import { createStore } from "redux";
 import counterReducer from "./reducer.js";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(counterReducer, composeWithDevTools());
+const createStore = reducer => {
+  let state;
+  let listeners = []; // keep track of all change listeners
+
+  const getState = () => state; // return the current state
+
+  const dispatch = action => {
+      state = reducer(state, action); // to calculate the new state we call the reducer with the current state and the action dispatched
+      listeners.forEach(listener => listener()); // update listeners after updating state 
+  };
+
+  const subscribe = listener => {
+    listeners.push(listener);
+    return () => { // instead of writing unsubscribe, return a function that removes the listener. similar logic with unsubscribe 
+        listeners = listeners.filter(l => l !== listener);
+    }
+  };
+// by the time the store is returned, we want to have the initial state populated
+// so we dispatch a dummy action just to get the reducer to return the initial value
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
+
+const store = createStore(counterReducer); 
 
 const displayValue = () => {
-    document.body.innerText = store.getState();
-}
+  document.body.innerText = store.getState();
+};
 
 store.subscribe(displayValue);
 
