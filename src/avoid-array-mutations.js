1fbe9dc1d289ef0edd2cae8f7dbd0833b0e4eddd
collinewait <collinewait17@gmail.com>
2019-06-23 13:15:18 +0300

    use slice instead of splice

--- src/avoid-array-mutations.js
+++ src/avoid-array-mutations.js
@@ -1,8 +1,7 @@
 export const addCounter = list => {
-  return list.concat([...list, 0]); //use concat which does not mutate the original array
+  return list.concat([...list, 0]); //use concat or spread operator which does not mutate the original array
 };
 
 export const removeCounter = (list, index) => {
-  list.splice(index, 1);
-  return list;
+  return list.slice(0, index).concat(list.slice(index + 1)); // use slice which does not mutate the original array like splice
 };
--- src/avoid-array-mutations.test.js
+++ src/avoid-array-mutations.test.js
@@ -12,6 +12,8 @@ it("adds a number to the list without mutating the original array", () => {
 it("removes a number from an array without mutating the original array", () => {
   const listBefore = [0, 10, 20];
   const listAfter = [0, 20];
+  
+  deepFreeze(listBefore);
 
   expect(removeCounter(listBefore, 1)).toEqual(listAfter);
 });
