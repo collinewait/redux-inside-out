26122ab91cbe7c0af42d695e82c3592b9fdcf581
collinewait <collinewait17@gmail.com>
2019-06-23 13:27:05 +0300

    update the code with a spread operator

--- src/avoid-array-mutations.js
+++ src/avoid-array-mutations.js
@@ -7,8 +7,5 @@ export const removeCounter = (list, index) => {
 };
 
 export const incrementCounter = (list, index) => {
-  return list
-    .slice(0, index)
-    .concat(list[index] + 1)
-    .concat(list.slice(index + 1));
+  return [...list.slice(0, index), list[index] + 1, ...list.slice(index + 1)];
 };
