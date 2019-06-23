951fffee7d975d974ca67a419420446b4929bac8
collinewait <collinewait17@gmail.com>
2019-06-23 13:37:06 +0300

    toggle todo

---
+++ src/avoid-object-mutations.test.js
@@ -0,0 +1,17 @@
+import deepFreeze from "deep-freeze";
+import { toggleTodo } from "./avoid-object-mutations";
+
+it("toggles todo without mutating the object", () => {
+  const todoBefore = {
+    id: 0,
+    text: "Redux inside out",
+    completed: false
+  };
+  const todoAfter = {
+    id: 0,
+    text: "Redux inside out",
+    completed: true
+  };
+
+  expect(toggleTodo(todoBefore)).toEqual(todoAfter);
+});
