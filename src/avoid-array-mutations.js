2fd2e57d202a806270b4553fc2dc1f1f98a1c9da
collinewait <collinewait17@gmail.com>
2019-06-23 13:16:38 +0300

    use spread instead of concat

--- src/avoid-array-mutations.js
+++ src/avoid-array-mutations.js
@@ -3,5 +3,5 @@ export const addCounter = list => {
 };
 
 export const removeCounter = (list, index) => {
-  return list.slice(0, index).concat(list.slice(index + 1)); // use slice which does not mutate the original array like splice
+  return [...list.slice(0, index), ...list.slice(index + 1)]; // use slice which does not mutate the original array like splice
 };
