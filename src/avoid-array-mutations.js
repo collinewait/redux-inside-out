840ab97ad4656d498b00141107abff9d544b2364
collinewait <collinewait17@gmail.com>
2019-06-23 13:23:01 +0300

    increment counter

--- src/avoid-array-mutations.js
+++ src/avoid-array-mutations.js
@@ -5,3 +5,8 @@ export const addCounter = list => {
 export const removeCounter = (list, index) => {
   return [...list.slice(0, index), ...list.slice(index + 1)]; // use slice which does not mutate the original array like splice
 };
+
+export const incrementCounter = (list, index) => {
+  list[index]++;
+  return list;
+};
--- src/avoid-array-mutations.test.js
+++ src/avoid-array-mutations.test.js
@@ -1,5 +1,9 @@
 import deepFreeze from "deep-freeze";
-import { addCounter, removeCounter } from "./avoid-array-mutations";
+import {
+  addCounter,
+  removeCounter,
+  incrementCounter
+} from "./avoid-array-mutations";
 
 it("adds a number to the list without mutating the original array", () => {
   const listBefore = [];
@@ -12,8 +16,15 @@ it("adds a number to the list without mutating the original array", () => {
 it("removes a number from an array without mutating the original array", () => {
   const listBefore = [0, 10, 20];
   const listAfter = [0, 20];
-  
+
   deepFreeze(listBefore);
 
   expect(removeCounter(listBefore, 1)).toEqual(listAfter);
+});
+
+it("increments counter", () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+
+  expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
 });
