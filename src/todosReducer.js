891dc2cf99939fa8b2d04b4725012b66000ee09b
collinewait <collinewait17@gmail.com>
2019-06-24 15:14:33 +0300

    normalize the state shape

--- src/todosReducer.js
+++ src/todosReducer.js
@@ -1,42 +1,39 @@
 import { combineReducers } from "redux";
+import todo from "./todo";
 //  creating and updating a todo in response to an action is
 // is a separate operation and needs to be handles by a separate
 //function called "todo" Pattern: Reducer composition
-const todo = (state, action) => {
+export const byId = (state = {}, action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      };
-
     case "TOGGLE_TODO":
-      if (state.id !== action.id) {
-        return state;
-      }
       return {
+        // return a new lookup table where the value under the id in the action
+        // is going to be the result of calling the reducer on the previous value under this id and the action
         ...state,
-        completed: !state.completed
+        [action.id]: todo(state[action.id], action)
       };
 
     default:
       return state;
   }
 };
-export const todos = (state = [], action) => {
+
+// keeps track of all the added Ids
+const allIds = (state = [], action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return [...state, todo(undefined, action)];
-
-    case "TOGGLE_TODO":
-      return state.map(t => todo(t, action));
-
+      return [...state, action.id];
     default:
       return state;
   }
 };
 
+const todos = combineReducers({
+  // You can use several combineReducers in several places
+  byId,
+  allIds
+});
 // export const todoApp = (state = {}, action) => {
 //     return {
 //         todos: todos(
@@ -55,25 +52,29 @@ export const todos = (state = [], action) => {
 //     return Object.keys(reducers).reduce((nextState, key) => {
 //       nextState[key] = reducers[key](state[key], action);
 //       return nextState; // return the next accumulated value
-//     }, {}); // return the initial next state {} before all the keys are processed. {} is to be filled gradually by reduce  
+//     }, {}); // return the initial next state {} before all the keys are processed. {} is to be filled gradually by reduce
 //   };
 // };
+const getAllTodos = state =>
+  state.allIds.map(id => state.byId[id]);
 
 export const getVisibleTodosImpl = (state, filter) => {
-    switch (filter) {
-      case "all":
-        return state;
-      case "completed":
-        return state.filter(todo => todo.completed);
-      case "active":
-        return state.filter(todo => !todo.completed);
-      default:
-          throw new Error(`Unknown filter: ${filter}`)
-    }
-  };
+  const allTodos = getAllTodos(state);
+  switch (filter) {
+    case "all":
+      return allTodos;
+    case "completed":
+      return allTodos.filter(todo => todo.completed);
+    case "active":
+      return allTodos.filter(todo => !todo.completed);
+    default:
+      throw new Error(`Unknown filter: ${filter}`);
+  }
+};
 
-export const getVisibleTodos = (state, filter) => getVisibleTodosImpl(state.todos, filter)
+export const getVisibleTodos = (state, filter) =>
+  getVisibleTodosImpl(state.todos, filter);
 
 export const todoApp = combineReducers({
-  todos,
+  todos
 });
