4a5c9bfd30f8175698038ffb8d6afa3db83f09ec
collinewait <collinewait17@gmail.com>
2019-06-24 17:38:43 +0300

    rename rawDispatch to next

--- src/configureStore.js
+++ src/configureStore.js
@@ -66,25 +66,24 @@ import { composeWithDevTools } from "redux-devtools-extension";
 
 // Now days we can easily use redux devtools
 const addPromiseSupportToDispatch = store => {
-  const rawDispatch = store.dispatch;
+  const next= store.dispatch; // next points to the dispatch returned by addLoggingToDispatch
   return action => {
-    console.log('Actionnnn', action)
     if (typeof action.then === "function") {
-      return action.then(rawDispatch);
+      return action.then(next);
     }
-    return rawDispatch(action);
+    return next(action);
   };
 };
 const addLoggingToDispatch = store => {
-  const rawDispatch = store.dispatch;
+  const next = store.dispatch; // this next points to the original store dispatch
   if (!console.group) {
-    return rawDispatch;
+    return next;
   }
   return action => {
     console.group(action.type);
     console.log("%c prev state", "color: gray", store.getState());
     console.log("%c action", "color: blue", action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log("%c next state", "color: green", store.getState());
     console.groupEnd(action.type);
     return returnValue;
