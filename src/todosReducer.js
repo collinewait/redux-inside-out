a37fc91306186a68b50666fca03435e4dfa53147
collinewait <collinewait17@gmail.com>
2019-06-24 19:56:26 +0300

    update the state with fetched data

--- src/todosReducer.js
+++ src/todosReducer.js
@@ -1,19 +1,15 @@
 import { combineReducers } from "redux";
-import todo from "./todo";
 //  creating and updating a todo in response to an action is
 // is a separate operation and needs to be handles by a separate
 //function called "todo" Pattern: Reducer composition
 export const byId = (state = {}, action) => {
   switch (action.type) {
-    case "ADD_TODO":
-    case "TOGGLE_TODO":
-      return {
-        // return a new lookup table where the value under the id in the action
-        // is going to be the result of calling the reducer on the previous value under this id and the action
-        ...state,
-        [action.id]: todo(state[action.id], action)
-      };
-
+    case "RECEIVE_TODOS":
+      const nextState = { ...state };
+      action.response.forEach(todo => {
+        nextState[todo.id] = todo;
+      });
+      return nextState;
     default:
       return state;
   }
@@ -21,55 +17,56 @@ export const byId = (state = {}, action) => {
 
 // keeps track of all the added Ids
 const allIds = (state = [], action) => {
+  if (action.filter !== "all") {
+    return state;
+  }
   switch (action.type) {
-    case "ADD_TODO":
-      return [...state, action.id];
+    case "RECEIVE_TODOS":
+      return action.response.map(todo => todo.id);
     default:
       return state;
   }
 };
 
+const activeIds = (state = [], action) => {
+  if (action.filter !== "active") {
+    return state;
+  }
+  switch (action.type) {
+    case "RECEIVE_TODOS":
+      return action.response.map(todo => todo.id);
+    default:
+      return state;
+  }
+};
+
+const completedIds = (state = [], action) => {
+  if (action.filter !== "completed") {
+    return state;
+  }
+  switch (action.type) {
+    case "RECEIVE_TODOS":
+      return action.response.map(todo => todo.id);
+    default:
+      return state;
+  }
+};
+
+const idsByFilter = combineReducers({
+  all: allIds,
+  active: activeIds,
+  completed: completedIds
+});
+
 const todos = combineReducers({
   // You can use several combineReducers in several places
   byId,
-  allIds
+  idsByFilter
 });
-// export const todoApp = (state = {}, action) => {
-//     return {
-//         todos: todos(
-//             state.todos,
-//             action
-//         ),
-//         visibilityFilter: visibilityFilter(
-//             state.visibilityFilter,
-//             action
-//         )
-//     }
-// }
-
-// const combineReducers = reducers => {
-//   return (state = {}, action) => {// the return value is supposed to be the reducer itself
-//     return Object.keys(reducers).reduce((nextState, key) => {
-//       nextState[key] = reducers[key](state[key], action);
-//       return nextState; // return the next accumulated value
-//     }, {}); // return the initial next state {} before all the keys are processed. {} is to be filled gradually by reduce
-//   };
-// };
-const getAllTodos = state =>
-  state.allIds.map(id => state.byId[id]);
 
 export const getVisibleTodosImpl = (state, filter) => {
-  const allTodos = getAllTodos(state);
-  switch (filter) {
-    case "all":
-      return allTodos;
-    case "completed":
-      return allTodos.filter(todo => todo.completed);
-    case "active":
-      return allTodos.filter(todo => !todo.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}`);
-  }
+  const ids = state.idsByFilter[filter];
+  return ids.map(id => state.byId[id]);
 };
 
 export const getVisibleTodos = (state, filter) =>
